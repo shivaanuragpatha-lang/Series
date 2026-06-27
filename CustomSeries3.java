af5fdb0b8a529107031cf7fb9d5d5116d646e5c6
shivaanuragpatha-lang <shivaanuragpatha@gmail.com>
2026-06-27 17:35:13 +0530

    Update CustomSeries3.java

--- CustomSeries3.java
+++ CustomSeries3.java
@@ -1,21 +1,27 @@
+/*printing ccustom series in the following format : 11,28,327,464,5125,......*/
 package com.java.series;
 import java.util.Scanner;
 public class CustomSeries3 {
     static void main() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String num = "0";
-        int count = 0;
+        long num = 0;
+        int count;
+        int ressult;
         for (int i = 1; i <= n; i++) {
             int result = i*i*i;
-            int digit = result % 10;
-            result = result / 10;
+            count = 0;
+            int temp = result;
+            while(temp>0){
+            int digit = temp % 10;
             count = count + 1;
-            for (int j = 1; j <= count; j++) {
-                num =  num + "0";
+            temp = temp / 10;
             }
-            num = (num) + result;
-            System.out.println(num+", ");
+            num = (long)i*(long)Math.pow(10,count) + result;  //The resultant number might lead to a large number. So, it need to be converted into long datatype.
+            System.out.print(num);
+            if(i<n){
+                System.out.print(",");
+            }
         }
         sc.close();
     }
